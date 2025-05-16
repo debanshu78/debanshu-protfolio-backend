d97e0556d98cdd7ede8978b1a00ce5acf693f575
debanshu <debanshu78@gmail.com>
2025-05-17 02:23:06 +0530

    feat: implement initial server setup with express, add dotenv for environment variables, and create route files for users, feedback, skills, and testimonials

--- server.js
+++ server.js
@@ -1,17 +1,7 @@
-const express = require("express");
-const cors = require("cors");
+import "dotenv/config";
+import app from "./app.js";
 
-
-const app=express();
-
-const port=process.env.PORT || 5000;
-
-
-
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
-
-app.listen(port,()=>{
-    console.log(`server is running on port ${port}`);
-})
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
